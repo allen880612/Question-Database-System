08b29c1d5ba76fa0ae3b3df9a9cca57e3785d1b8
Raven9527 <acd667y@gmail.com>
2019-07-04 01:15:58 +0800

    QDS Ver 0.0.7 : * update ui *

--- view/temp_test.py
+++ view/temp_test.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'D:\QDS\view\test.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
 from PyQt5 import QtCore, QtGui, QtWidgets
-import sys
-sys.path.append('../controller')
-sys.path.append('../model')
-from controller import GET_PATH
-from model import FetchData
-import os
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -13,7 +15,7 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(190, 50, 271, 71))
+        self.label.setGeometry(QtCore.QRect(60, 50, 571, 71))
         font = QtGui.QFont()
         font.setFamily("Consolas")
         font.setPointSize(26)
@@ -22,93 +24,44 @@ class Ui_MainWindow(object):
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.btn_confirm = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_confirm.setGeometry(QtCore.QRect(180, 260, 75, 23))
+        self.btn_confirm.setGeometry(QtCore.QRect(160, 300, 75, 23))
         self.btn_confirm.setObjectName("btn_confirm")
-        self.group_1 = QtWidgets.QGroupBox(self.centralwidget)
-        self.group_1.setGeometry(QtCore.QRect(90, 140, 120, 80))
-        self.group_1.setObjectName("group_1")
-        self.rb_basic = QtWidgets.QRadioButton(self.group_1)
-        self.rb_basic.setGeometry(QtCore.QRect(10, 20, 83, 16))
-        self.rb_basic.setCheckable(True)
-        self.rb_basic.setChecked(False)
-        self.rb_basic.setObjectName("rb_basic")
-        self.rb_select = QtWidgets.QRadioButton(self.group_1)
-        self.rb_select.setGeometry(QtCore.QRect(10, 50, 83, 16))
-        self.rb_select.setObjectName("rb_select")
-        self.group_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.group_2.setGeometry(QtCore.QRect(240, 140, 120, 80))
-        self.group_2.setObjectName("group_2")
-        self.rb_first = QtWidgets.QRadioButton(self.group_2)
-        self.rb_first.setGeometry(QtCore.QRect(10, 20, 83, 16))
-        self.rb_first.setCheckable(True)
-        self.rb_first.setChecked(False)
-        self.rb_first.setObjectName("rb_first")
-        self.rb_second = QtWidgets.QRadioButton(self.group_2)
-        self.rb_second.setGeometry(QtCore.QRect(10, 50, 83, 16))
-        self.rb_second.setObjectName("rb_second")
+        self.cBox_dir1 = QtWidgets.QComboBox(self.centralwidget)
+        self.cBox_dir1.setGeometry(QtCore.QRect(60, 200, 121, 21))
+        self.cBox_dir1.setObjectName("cBox_dir1")
+        self.cBox_dir2 = QtWidgets.QComboBox(self.centralwidget)
+        self.cBox_dir2.setGeometry(QtCore.QRect(210, 200, 121, 21))
+        self.cBox_dir2.setObjectName("cBox_dir2")
+        self.cBox_word = QtWidgets.QComboBox(self.centralwidget)
+        self.cBox_word.setGeometry(QtCore.QRect(360, 200, 191, 21))
+        self.cBox_word.setObjectName("cBox_word")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(60, 180, 91, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(210, 180, 81, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(360, 180, 71, 16))
+        self.label_4.setObjectName("label_4")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 25))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.InitGUI()
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "選我啊"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Question Database"))
+        self.label.setText(_translate("MainWindow", "請選擇試題範圍"))
         self.btn_confirm.setText(_translate("MainWindow", "確定"))
-        self.group_1.setTitle(_translate("MainWindow", "科目"))
-        self.rb_basic.setText(_translate("MainWindow", "基礎化學"))
-        self.rb_select.setText(_translate("MainWindow", "選修化學"))
-        self.group_2.setTitle(_translate("MainWindow", "年度"))
-        self.rb_first.setText(_translate("MainWindow", "上"))
-        self.rb_second.setText(_translate("MainWindow", "下"))
-
-    def InitGUI(self):
-        self.btn_confirm.clicked.connect(self.Confirm)
-
-
-    def Confirm(self):
-        path = []
-        # 選科目
-        if self.rb_basic.isChecked():
-            path.append("基礎化學")
-        elif self.rb_select.isChecked():
-            path.append("選修化學")
-        else:
-            self.label.setText("請選擇科目!")
-            return
-        # 選學年
-        if self.rb_first.isChecked():
-            path.append("上")
-        elif self.rb_second.isChecked():
-            path.append("下")
-        else:
-            self.label.setText("請選擇學年!")
-            return
-
-        parser = GET_PATH.PathPaser(path)
-        aim_path = parser.GetPath()
-
-        print("path = " + aim_path)
-        # # testPath = "../database/選修化學/上/"
-        # try:
-        #     print(os.listdir(aim_path))
-        # except:
-        #     print("listdir 爆炸了")
-
-        # 透過 Model拿
-        wManager = FetchData.WordManager(aim_path)
-        aim_datas = wManager.ReadWord()
-
-
-
-
+        self.label_2.setText(_translate("MainWindow", "資料夾 一層"))
+        self.label_3.setText(_translate("MainWindow", "資料夾 二層"))
+        self.label_4.setText(_translate("MainWindow", "Word"))
 
